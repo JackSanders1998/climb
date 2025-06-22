1d79c79add1374d17f7b9dd2f093acc5836be72b
Jack Sanders <jacklewissanders@gmail.com>
2025-06-22 17:09:19 -0500

    chat hello world

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,56 @@
 import { api } from "@/convex/_generated/api";
-import { useQuery } from "convex/react";
-import { Text, View } from "react-native";
+import { useMutation, useQuery } from "convex/react";
+import React, { useState } from "react";
+import { FlatList, SafeAreaView, Text, TextInput, View } from "react-native";
+import styles from "./styles";
 
 export default function Index() {
-  const tasks = useQuery(api.tasks.get);
+  const messages = useQuery(api.messages.list) || [];
+
+  const [newMessageText, setNewMessageText] = useState("");
+  const sendMessage = useMutation(api.messages.send);
+
+  const [name] = useState(() => "User " + Math.floor(Math.random() * 10000));
+  async function handleSendMessage(event: { preventDefault: () => void }) {
+    event.preventDefault();
+    setNewMessageText("");
+    await sendMessage({ body: newMessageText, author: name });
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      {tasks?.map(({ _id, text }) => <Text key={_id}>{text}</Text>)}
-    </View>
+    <SafeAreaView style={styles.body}>
+      <Text style={styles.title}>Convex Chat</Text>
+      <View style={styles.name}>
+        <Text style={styles.nameText} testID="NameField">
+          {name}
+        </Text>
+      </View>
+      <FlatList
+        data={messages.slice(-10)}
+        testID="MessagesList"
+        renderItem={(x) => {
+          const message = x.item;
+          return (
+            <View style={styles.messageContainer}>
+              <Text>
+                <Text style={styles.messageAuthor}>{message.author}:</Text>{" "}
+                {message.body}
+              </Text>
+              <Text style={styles.timestamp}>
+                {new Date(message._creationTime).toLocaleTimeString()}
+              </Text>
+            </View>
+          );
+        }}
+      />
+      <TextInput
+        placeholder="Write a message…"
+        style={styles.input}
+        onSubmitEditing={handleSendMessage}
+        onChangeText={(newText) => setNewMessageText(newText)}
+        defaultValue={newMessageText}
+        testID="MessageInput"
+      />
+    </SafeAreaView>
   );
 }
