d0cb5ddaf48c33ab9fbf20b02da1b9dbdc6f0d9f
Pablo Olavarrieta <pablo.olavarr@gmail.com>
2025-07-15 20:21:26 -0700

    fix: use custom text component for consistency

--- lib/components/Toggle.tsx
+++ lib/components/Toggle.tsx
@@ -1,5 +1,7 @@
 import { Switch } from "@expo/ui/swift-ui";
 import { sandA } from "@radix-ui/colors";
+import { View } from "react-native";
+import { Text } from "../ui/Text";
 
 export const Toggle = ({
   checked,
@@ -11,14 +13,24 @@ export const Toggle = ({
   label: string;
 }) => {
   return (
-    <Switch
-      value={checked}
-      onValueChange={(checked) => {
-        setChecked(checked);
+    <View
+      style={{
+        flexDirection: "row",
+        width: "100%",
+        justifyContent: "space-between",
+        alignItems: "center",
       }}
-      color={sandA.sandA12}
-      label={label}
-      variant="switch"
-    />
+    >
+      <Text>{label}</Text>
+      <Switch
+        value={checked}
+        onValueChange={(checked) => {
+          setChecked(checked);
+        }}
+        color={sandA.sandA12}
+        label={label}
+        variant="switch"
+      />
+    </View>
   );
 };
