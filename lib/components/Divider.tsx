1a55eb4ac7e66cb5d0c9234d3dd1c1812073658b
Pablo Olavarrieta <pablo.olavarr@gmail.com>
2025-07-16 22:12:03 -0700

    refactor: extract out divider comp

---
+++ lib/components/Divider.tsx
@@ -0,0 +1,15 @@
+import { sandA } from "@radix-ui/colors";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+export const Divider = () => {
+  return (
+    <View
+      style={{
+        height: StyleSheet.hairlineWidth,
+        backgroundColor: sandA.sandA6,
+        width: "100%",
+      }}
+    />
+  );
+};
