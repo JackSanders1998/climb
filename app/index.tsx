577bfd6cc756c501b14bbda5849251439dd32f9a
Pablo Olavarrieta <pablo.olavarr@gmail.com>
2025-07-17 22:22:28 -0700

    idk: this gradient blur looks atrocious

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,7 @@
 import { ActiveSessionFooter } from "@/lib/components/ActiveSessionFooter";
 import SillyNav from "@/lib/components/SillyNav";
+import { Glur } from "@/lib/ui/Glur";
+
 import React, { useState } from "react";
 import { View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
@@ -11,11 +13,11 @@ export default function Summary() {
 
   return (
     <>
-      <SillyNav pageName="Summary" paddingBottom={footerHeight} />
+      <SillyNav pageName="Summary" paddingBottom={footerHeight - bottom} />
 
       <View
         style={{
-          bottom,
+          bottom: 0,
           position: "absolute",
           left: 8,
           right: 8,
@@ -23,9 +25,15 @@ export default function Summary() {
           justifyContent: "center",
           flexDirection: "row",
           gap: 8,
+          padding: 24,
+          paddingBottom: bottom + 24,
         }}
         onLayout={(e) => setFooterHeight(e.nativeEvent.layout.height)}
       >
+        <Glur
+          direction="b2t"
+          style={{ position: "absolute", bottom: 0, left: 0, right: 0 }}
+        />
         <ActiveSessionFooter />
       </View>
     </>
